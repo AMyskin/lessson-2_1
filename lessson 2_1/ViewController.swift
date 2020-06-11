d21e88a62965c9de2a8df2f616aa3b08abe87f9d
Alexander Myskin <amyskin@yandex.ru>
2020-06-11 20:06:03 +0300

    Initial Commit

---
+++ lessson 2_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lessson 2_1
+//
+//  Created by Alexander Myskin on 11.06.2020.
+//  Copyright © 2020 Alexander Myskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
