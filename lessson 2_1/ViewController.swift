41e969bf1214db5b982807385401e4ab762d25b4
Alexander Myskin <amyskin@yandex.ru>
2020-06-15 13:54:03 +0300

    create

--- lessson 2_1/ViewController.swift
+++ lessson 2_1/ViewController.swift
@@ -8,11 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func signinPressed(_ sender: UIButton){
+        guard let login = loginTextField.text, let password = passwordTextField.text else {return}
+        if login == "admin" && password == "123456" {
+            print("успешная авторизация")
+        } else {
+            print("неуспешная авторизация")
+        }
+        loginTextField.text = ""
+        passwordTextField.text = ""
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loginTextField.delegate = self
+        passwordTextField.delegate = self
+
+    }
+    
+    @objc func keyboardWillShow(notification: Notification){
+        guard let keybordSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {return}
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: keybordSize.height, right: 0)
+        scrollView.contentInset = insets
+    }
+    @objc func keyboardWillHide(notification: Notification){
+        scrollView.contentInset = .zero
+    }
+    
+    @IBAction func scrollTapped(_ gesture:UIGestureRecognizer){
+        scrollView.endEditing(true)
+    }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.removeObserver(self)
     }
 
 
